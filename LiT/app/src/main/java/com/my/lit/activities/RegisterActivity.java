be4ada28ac78144371610918e70292ebaf180fc7
Rahul Dev <iit2019053@iiita.ac.in>
2021-03-11 13:46:57 +0530

    progress dialog box

--- LiT/app/src/main/java/com/my/lit/activities/RegisterActivity.java
+++ LiT/app/src/main/java/com/my/lit/activities/RegisterActivity.java
@@ -2,8 +2,10 @@ package com.my.lit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -15,6 +17,7 @@ import com.my.lit.databinding.ActivityRegisterBinding;
 public class RegisterActivity extends AppCompatActivity {
 
     ActivityRegisterBinding activityRegisterBinding;
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +29,23 @@ public class RegisterActivity extends AppCompatActivity {
 
         activityRegisterBinding.signUpRegister.setOnClickListener(this::onClick);
         activityRegisterBinding.signUpRegister.setOnClickListener(this::onClick);
+        mProgress = new ProgressDialog(this);
     }
 
     private void onClick(View view) {
         switch (view.getId()){
             case R.id.signUp_register:
-                Intent intent = new Intent(RegisterActivity.this , DashBoard.class);
-                view.getContext().startActivity(intent);
+                mProgress.setTitle("Registering user");
+                mProgress.setMessage("Please wait while we Register you");
+                mProgress.setCanceledOnTouchOutside(false);
+                mProgress.show();
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        mProgress.dismiss();
+                        Intent intent = new Intent(RegisterActivity.this , DashBoard.class);
+                        view.getContext().startActivity(intent);
+                    }
+                }, 3000);
             case R.id.signIn:
                 Intent intent1 = new Intent(RegisterActivity.this , GuestLoginActivity.class);
                 view.getContext().startActivity(intent1);
--- LiT/app/src/main/java/com/my/lit/activities/login/GuestLoginActivity.java
+++ LiT/app/src/main/java/com/my/lit/activities/login/GuestLoginActivity.java
@@ -2,10 +2,13 @@ package com.my.lit.activities.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.my.lit.R;
 import com.my.lit.activities.DashBoard;
@@ -15,6 +18,7 @@ import com.my.lit.databinding.ActivityLoginBinding;
 public class GuestLoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding activityLoginBinding;
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class GuestLoginActivity extends AppCompatActivity {
 
         activityLoginBinding.SignUp.setOnClickListener(this::onClick);
         activityLoginBinding.signInButton.setOnClickListener(this::onClick);
+        mProgress = new ProgressDialog(this);
     }
 
     private void onClick(View view) {
@@ -34,8 +39,17 @@ public class GuestLoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(GuestLoginActivity.this , RegisterActivity.class);
                 view.getContext().startActivity(intent);
             case R.id.signIn:
-                Intent intent1 = new Intent(GuestLoginActivity.this , DashBoard.class);
-                view.getContext().startActivity(intent1);
+                mProgress.setTitle("Logging in");
+                mProgress.setMessage("Please wait while we fetching your login account");
+                mProgress.setCanceledOnTouchOutside(false);
+                mProgress.show();
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        mProgress.dismiss();
+                        Intent intent1 = new Intent(GuestLoginActivity.this , DashBoard.class);
+                        view.getContext().startActivity(intent1);
+                    }
+                }, 3000);
         }
     }
 }
