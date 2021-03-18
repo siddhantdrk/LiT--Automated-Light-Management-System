fb4e79802f4dd0f6ba4058d2e430a96ed8856835
Rahul Dev <iit2019053@iiita.ac.in>
2021-03-18 08:54:55 +0530

    add login function

--- LiT/app/src/main/java/com/my/lit/activities/login/GuestLoginActivity.java
+++ LiT/app/src/main/java/com/my/lit/activities/login/GuestLoginActivity.java
@@ -6,13 +6,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.my.lit.R;
 import com.my.lit.activities.dashboard.GuestDashBoardActivity;
 import com.my.lit.activities.register.GuestRegisterActivity;
+import com.my.lit.api.RetrofitClient;
 import com.my.lit.databinding.ActivityLoginBinding;
+import com.my.lit.responses.UserLoginResponse;
+import com.my.lit.responses.UserRegisterResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GuestLoginActivity extends AppCompatActivity {
 
@@ -43,20 +51,37 @@ public class GuestLoginActivity extends AppCompatActivity {
                 mProgress.setMessage("Please wait while we fetching your login account");
                 mProgress.setCanceledOnTouchOutside(false);
                 mProgress.show();
-                new Handler().postDelayed(new Runnable() {
-                    public void run() {
-                        mProgress.dismiss();
-                        Intent intent1 = new Intent(GuestLoginActivity.this, GuestDashBoardActivity.class);
-                        view.getContext().startActivity(intent1);
-                    }
-                }, 3000);
-                Intent intent1 = new Intent(GuestLoginActivity.this, GuestDashBoardActivity.class);
-                view.getContext().startActivity(intent1);
-                break;
+                login();
         }
     }
 
     private void login() {
+        String email = activityLoginBinding.signInEmail.getText().toString().trim();
+        String password = activityLoginBinding.signInPassword.getText().toString().trim();
+        Call<UserLoginResponse> userLoginResponseCall = RetrofitClient.getInstance().getUserServices().userLogin(email,password);
+        userLoginResponseCall.enqueue(new Callback<UserLoginResponse>() {
+            @Override
+            public void onResponse(Call<UserLoginResponse> call, Response<UserLoginResponse> response) {
+                mProgress.dismiss();
+                UserLoginResponse loginResponseCall = response.body();
+                if (response.isSuccessful()) {
+                    if (response.code() == 400) {
+                        Toast.makeText(GuestLoginActivity.this, loginResponseCall.getError(), Toast.LENGTH_SHORT).show();
+                    } else if (response.code() == 200) {
+                        startActivity(new Intent(GuestLoginActivity.this, GuestDashBoardActivity.class));
+                        Toast.makeText(GuestLoginActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                } else {
+                    Toast.makeText(GuestLoginActivity.this, "Unsuccessful " + response.code() + response.raw(), Toast.LENGTH_SHORT).show();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<UserLoginResponse> call, Throwable t) {
+                mProgress.dismiss();
+                Toast.makeText(GuestLoginActivity.this, "Something went wrong\n" + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
